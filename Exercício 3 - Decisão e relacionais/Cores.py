fc03a5bb2f0eb2c1979397de89cc8a7699fba2d7
joaolamin-dev <joaolamin1604@gmail.com>
2025-10-16 19:58:34 -0300

    Delete Exercício 3 - Decisão e relacionais directory

--- Exercício 3 - Decisão e relacionais/Cores.py
+++
@@ -1,9 +0,0 @@
-cor = input("Digite uma cor primária: ")
-if cor != "amarelo" and cor != "azul" and cor != "vermelho":
-  print("Essa cor não faz parte das primárias!")
-elif cor == "amarelo":
-  print("🟡 AMARELO: alegria, riqueza, luz")
-elif cor == "vermelho":
-  print("🔴 VERMELHO: calor, energia, perigo")
-elif cor == "azul":
-  print("🔵 AZUL: calma, inteligência, frio")
